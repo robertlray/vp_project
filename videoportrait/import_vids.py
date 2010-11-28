73db803b67956ff00fdf787cac90b8b7ab5212d0
robertlray <robert.l.ray@gmail.com>
2010-11-28 18:21:36 -0500

    Added import_vids file. Just added dictionary

---
+++ videoportrait/import_vids.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+categories = {'firstvideo.txt': 18,
+              'americanartists.txt': 17,
+              'civilrights.txt': 16,
+              'familyportraits.txt': 15,
+              'neighborhood.txt': 14,
+              'selfportraits.txt': 13,
+              'portraitsforinstallation.txt': 12,
+              'magritte.txt': 11,
+              'berlinseries.txt': 10,
+              'traditional_dance.txt': 9,
+              'valentine.txt': 8,
+              'newyork.txt': 7,
+              'frenchartists.txt': 6,
+              'sanfrancisco.txt': 5,
+              'frenchwriters.txt': 4,
+              'newengland.txt': 3,
+              'fivecomposers.txt': 2,
+              'digitallimmings.txt': 1
+              }
+# f = open('categoriesTXT/firstvideo.txt')
+z = f.readlines()
+for x in z:
+    y = x.split(',')
+    if len(y) >= 4:
+        title = y[0].strip('"')
+        dateline = y[1].strip('"')
+        videofilename = y[2].strip('"')
+        thumbnailfilename = y[3].strip().strip('"')
+        if len(y) == 5:
+            aspectratio = y[4].strip().strip('"')
+        else:
+            aspectratio = ""
+        print "title = %s, dateline = %s, videofilename = %s, thumbnailfilename = %s, aspectratio = %s" % (title,dateline,videofilename,thumbnailfilename,aspectratio)
+    
