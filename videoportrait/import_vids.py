5295a4f08e9401410b84ef7fa0080a7701cdf82a
robertlray <robert.l.ray@gmail.com>
2010-11-28 23:20:35 -0500

    updated import_vids.py

--- videoportrait/import_vids.py
+++ videoportrait/import_vids.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+from vp_project.models import Video, Category
+from django.shortcuts import get_object_or_404
+import datetime
+
 categories = {'firstvideo.txt': 18,
               'americanartists.txt': 17,
               'civilrights.txt': 16,
@@ -20,19 +24,24 @@ categories = {'firstvideo.txt': 18,
               'digitallimmings.txt': 1
               }
 for filename, category_id in categories.iteritems():
+    current_category = get_object_or_404(Category, pk=category_id)
     fn = 'categoriesTXT/' + filename
     f = open(fn)
     z = f.readlines()
     for x in z:
         y = x.split(',')
         if len(y) >= 4:
-            title = y[0].strip('"')
-            dateline = y[1].strip('"')
-            videofilename = y[2].strip('"')
-            thumbnailfilename = y[3].strip().strip('"')
+            new_vid = Video()
+            new_vid.title = y[0].strip('"')
+            new_vid.datetime = y[1].strip('"')
+            new_vid.description = ""
+            new_vid.shotdate = datetime.datetime.now()
+            new_vid.video_name = y[2].strip('"')
+            new_vid.thumbnail_name = y[3].strip().strip('"')
             if len(y) == 5:
-                aspectratio = y[4].strip().strip('"')
+                new_vid.aspectratio = y[4].strip().strip('"')
             else:
-                aspectratio = ""
-            print "title = %s, dateline = %s, videofilename = %s, thumbnailfilename = %s, aspectratio = %s" % (title,dateline,videofilename,thumbnailfilename,aspectratio)
+                new_vid.aspectratio = "0"
+            print "title = %s, datetime = %s, description = %s, shotdate = %s, videofname = %s, thumbnailfname = %s, aspectratio = %s" % (new_vid.title,new_vid.datetime,new_vid.description,new_vid.shotdate,new_vid.video_name,new_vid.thumbnail_name,new_vid.aspectratio)
+            new_video_object.save()
     
